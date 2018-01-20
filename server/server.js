ac521be405081a9b54b8e06983c39afb6b172a6f
= <andrespechtbs@googlemail.com>
2018-01-20 20:08:23 +0100

    create message generate utils

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {
+    generateMessage
+} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 
 var port = process.env.PORT || 3000;
@@ -13,33 +17,21 @@ var server = http.createServer(app);
 
 var io = socketIO(server);
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log("new connection");
 
 
-    socket.emit('newMessage', {
-        from: "Admin",
-        text: "Welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app"));
 
-    socket.broadcast.emit('newMessage',{
-        from: 'Admin',
-        text: 'New user has joined',
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
 
     socket.on('createMessage', (message) => {
-        console.log('Got Message:',message);
+        console.log('Got Message:', message);
 
-        io.emit('newMessage',{
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
-    socket.on('disconnect',() => {
+    socket.on('disconnect', () => {
         console.log('disconnected from client');
     });
 });
@@ -47,6 +39,6 @@ io.on('connection',(socket)=>{
 app.use(express.static(publicPath));
 
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log('server listening on port', port);
 })
---
+++ server/utils/message.test.js
@@ -0,0 +1,20 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+
+it('should generate the correct message object', () => {
+    
+    const from = "as@example.com";
+    const text = "how are you";
+
+    var res = generateMessage(from,text);
+
+    expect(res.from).toBe(from);
+    expect(res.text).toBe(text);
+    expect(res.createdAt).toBeA('number');
+
+});
+
+});
